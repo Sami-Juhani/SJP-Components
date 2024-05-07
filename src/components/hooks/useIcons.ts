54fcfb8f3de102b04ec988eb38eedcc190f7c8eb
Sami-Juhani <sami.paananen@gmail.com>
2024-05-07 20:11:54 +0300

    custom hook for icons

--- src/components/hooks/useIcons.ts
+++ src/components/hooks/useIcons.ts
@@ -1,5 +1,10 @@
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import WarningAmberIcon from "@mui/icons-material/WarningAmber";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import CloseIcon from "@mui/icons-material/Close";
 
 /**
  * A hook that provides access to specific SVG icons.
@@ -10,6 +15,10 @@ export default function useIcons() {
   /* ARROWS */
   const ArrowLeft = ArrowBackIosNewIcon;
   const ArrowRight = ArrowForwardIosIcon;
+  const SuccessIcon = CheckCircleOutlineIcon;
+  const ErrorIcon = ErrorOutlineIcon;
+  const WarningIcon = WarningAmberIcon;
+  const NotificationIcon = NotificationsNoneIcon;
 
-  return { arrows: { ArrowLeft, ArrowRight } };
+  return { arrows: { ArrowLeft, ArrowRight }, status: { SuccessIcon, ErrorIcon, WarningIcon, NotificationIcon }, action: { CloseIcon } };
 }
