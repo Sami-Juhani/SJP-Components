5bbf953d4bfdaa21da0bdcf062b200eead4bd72b
Sami-Juhani <sami.paananen@gmail.com>
2024-05-16 12:52:41 +0300

    add fancyhero-test-page

---
+++ src/pages/FancyHeroTest.tsx
@@ -0,0 +1,16 @@
+import { FancyHero } from "../components/FancyHero";
+import { TypeWriterOptions } from "../components/FancyHero/TypeWriter";
+
+export function FancyHeroTest() {
+  const imgSrc = "./bw-profile.jpg";
+  const userDefinedOptions: TypeWriterOptions = {
+    typingSpeed: 75,
+    x: "5%",
+    y: "10%",
+    startDelay: 2000,
+    endDelay: 2000,
+  };
+  const paragraph = ["I'm a frontend developer", "I'm a backend developer"];
+
+  return <FancyHero imgSrc={imgSrc} paragraph={paragraph} />;
+}
