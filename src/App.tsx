1edb680dacd53d5e7c6631cc2c2209bc45dd62d9
Sami-Juhani <sami.paananen@gmail.com>
2024-05-17 22:12:59 +0300

    testpage for StickyNav

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { MediaScrollerTest } from "./pages/MediaScrollerTest";
 import { NotificationModalTest } from "./pages/NotificationModalTest";
 import { InfiniteCarouselTest } from "./pages/InfiniteCarouselTest";
 import { FancyHeroTest } from "./pages/FancyHeroTest";
+import { NavBarTest } from "./pages/NavBarTest";
 
 function App() {
   return (
-    <main className="container-max-width">
-      <FancyHeroTest />
-      <NotificationModalTest />
-      <MediaScrollerTest />
-      <InfiniteCarouselTest />
+    <main className="main-container">
+      <NavBarTest />
+      <div className="container-max-width">
+        <FancyHeroTest />
+        <NotificationModalTest />
+        <MediaScrollerTest />
+        <InfiniteCarouselTest />
+      </div>
     </main>
   );
 }
